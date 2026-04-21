a5ba474315b9577dd1ea499b634f9d0d4d6c415c
Khanhbroo <khanhdq1120@gmail.com>
2026-04-21 11:06:10 +0700

    feat: add animated-tittle component using gsap for text animation

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,4 @@
 export { default as Hero } from "./Hero";
 export { default as Button } from "./Button";
 export { default as About } from "./About";
+export { default as AnimatedTitle } from "./AnimatedTitle";
